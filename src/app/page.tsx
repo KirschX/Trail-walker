3b7f0def6317028ac5e17efa61ca76717fcf79e8
KirschX <mk44879@gmail.com>
2023-10-11 23:07:57 +0900

    feat: implemented trailview with buttons on background images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import TrailView from "@/components/TrailView";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import scenes from "../../public/scenes.json";
 import Controller from "@/components/Controller";
 
 export default function Home() {
+  let scrollContainerRef = useRef<HTMLDivElement | null>(null);
   return (
-    <main>
-      <Controller />
-      <TrailView />
+    <main
+      ref={scrollContainerRef}
+      className=" h-[100vh] overflow-x-scroll overflow-y-hidden transition-all duration-1000"
+    >
+      <Controller scrollContainerRef={scrollContainerRef} />
+      <TrailView scrollContainerRef={scrollContainerRef} />
     </main>
   );
 }
---
+++ src/components/TrailView.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import Image from "next/image";
+import { useTrackingState } from "@/store/store";
+
+import DirectionIcon from "./DirectionIcon";
+
+import scenes from "../../public/scenes.json";
+
+import useMousePosition from "@/hooks/useMousePosition";
+import isWithinBoundary from "@/helper/isWithinBoundary";
+import { useEffect, useState } from "react";
+
+interface ControllerProps {
+  scrollContainerRef: React.RefObject<HTMLDivElement>;
+}
+
+export default function TrailView({ scrollContainerRef }: ControllerProps) {
+  const [windowHeight, setWindowHeight] = useState<number>(0);
+  const { currentLocation, updateTrackingState } = useTrackingState();
+  const { xy } = useMousePosition(scrollContainerRef);
+
+  const currentScene = scenes.scenes[currentLocation];
+  const hasMiddle = currentScene.hitzones.hasOwnProperty("middle");
+  const boundaries = determineBoundaries(hasMiddle);
+
+  const navigateTo = (destination: number) => {
+    updateTrackingState(destination);
+  };
+
+  const handleClick = () => {
+    if (isWithinBoundary(xy.xRatio, 10, boundaries.leftBoundary))
+      navigateTo(currentScene.hitzones.left.goto);
+    if (
+      hasMiddle &&
+      currentScene.hitzones.middle &&
+      isWithinBoundary(
+        xy.xRatio,
+        boundaries.leftBoundary,
+        boundaries.middleBoundary
+      )
+    )
+      navigateTo(currentScene.hitzones.middle.goto);
+    if (
+      currentScene.hitzones.right &&
+      isWithinBoundary(
+        xy.xRatio,
+        boundaries.middleBoundary || boundaries.leftBoundary,
+        boundaries.rightBoundary
+      )
+    )
+      navigateTo(currentScene.hitzones.right.goto);
+
+    return null;
+  };
+
+  useEffect(() => {
+    setWindowHeight(window.innerHeight);
+  }, []);
+
+  return (
+    <>
+      <DirectionIcon
+        x={xy.x}
+        y={xy.y}
+        screenX={xy.screenX}
+        screenY={xy.screenY}
+        xRatio={xy.xRatio}
+        boundaries={boundaries}
+        hasMiddle={hasMiddle}
+        screenXRatio={xy.screenXRatio}
+      />
+
+      <div
+        // onClick={handleClick}
+        className=" h-full aspect-[6.565/1] min-w-full relative"
+      >
+        {Object.entries(currentScene.hitzones).map(([key, value]) => {
+          const { pos } = value;
+          return (
+            <div key={key}>
+              <div
+                onClick={() => navigateTo(value.goto)}
+                className="absolute bg-red-500 z-20 p-2 rounded-full"
+                style={{
+                  left: (pos[0] * xy.scrollWidth) / 100,
+                  top: (pos[1] * windowHeight) / 100,
+                }}
+              >
+                Go
+              </div>
+            </div>
+          );
+        })}
+        <Image
+          className=" object-cover"
+          fill
+          src={currentScene.url}
+          alt="trail scene"
+        ></Image>
+      </div>
+    </>
+  );
+}
+
+function determineBoundaries(hasMiddle: boolean) {
+  if (hasMiddle)
+    return { leftBoundary: 33, middleBoundary: 66, rightBoundary: 97 };
+  return { leftBoundary: 50, rightBoundary: 97 };
+}
